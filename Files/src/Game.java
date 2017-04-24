801c67b30cf46ae5be3c060dfdff2e28af8ae4a6
HueMungus <joshuatime@windowslive.com>
2017-04-24 15:48:01 +1200

    stuff

---
+++ Files/src/Game.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Game extends JFrame {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
--- Files/src/Vector2.java
+++ Files/src/Vector2.java
@@ -39,7 +39,6 @@ public class Vector2 {
 		return this;
 	}
 	
-	// multThis is preferred as it does not create a new Vector2
 	public Vector2 multThis(double d) {
 		this.x = this.x * d;
 		this.y = this.y * d;
@@ -49,16 +48,6 @@ public class Vector2 {
 	public Vector2 mult(double d) {
 		return new Vector2(Math.round(this.x * d),Math.round(this.y * d));
 	}
-	
-	/*// float for x
-	public void add(float add) {
-		this.x += add;
-	}
-	
-	// int for y
-	public void add(int add) {
-		this.y += add;
-	}*/
 	
 	public static long DotProduct(Vector2 A, Vector2 B) {
 		return Math.round((A.x * B.x) + (A.y * B.y));
