d18ca80b2266739d5a573178a378537c3ca8c478
HueMungus <joshuatime@windowslive.com>
2017-05-01 20:23:29 +1200

    Added base 'block' classes

---
+++ Files/src/game/Block.java
@@ -0,0 +1,14 @@
+package game;
+
+import java.awt.Color;
+
+public class Block {
+	Color color;
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Files/src/game/StaticBlock.java
@@ -0,0 +1,5 @@
+package game;
+
+public class StaticBlock extends Block {
+
+}
