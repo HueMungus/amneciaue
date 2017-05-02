7521ef29cd0cf3a81605308ac4e0cad2bbbdab4d
john <john@localhost>
2017-05-02 15:03:55 +1200

    Added some stuff

---
+++ Files/src/game/Engine.java
@@ -0,0 +1,26 @@
+package game;
+
+import java.util.ArrayList;
+
+public class Engine extends GameEngine {
+	
+	// Goes through each array at the end of update loop
+	ArrayList<Block> toAdd = new ArrayList<Block>();
+	ArrayList<Block> toRemove = new ArrayList<Block>();
+	
+	
+	
+	
+	@Override
+	public void update(double dt) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void paintComponent() {
+		
+
+	}
+
+}
--- Files/src/game/HitBlock.java
+++
@@ -1,5 +0,0 @@
-package game;
-
-public class HitBlock extends Block {
-
-}
